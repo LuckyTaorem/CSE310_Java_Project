b651460b7908413c06b35ce31c9be51ca582a2c3
Taorem Lucky singh <luckytaorem5@gmail.com>
2023-03-25 10:16:23 +0530

    Questions Randomize each time

--- com.luckytaorem_quiz_application_jar_1.0-SNAPSHOT/src/main/java/com/luckytaorem/quiz_application/QuizTest.java
+++ com.luckytaorem_quiz_application_jar_1.0-SNAPSHOT/src/main/java/com/luckytaorem/quiz_application/QuizTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.luckytaorem.quiz_application;
+ package com.luckytaorem.quiz_application;
 //import java.awt.event.*;
 import javax.swing.*;
 /**
